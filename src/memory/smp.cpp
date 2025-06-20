e00bc1f70873c58b4ee3f74798225cd248d0f563
Siddharth Praveen Bharadwaj <srsfool@gmail.com>
2025-06-20 18:15:35 +0530

    Minor SMP fixes

--- src/memory/smp.cpp
+++ src/memory/smp.cpp
@@ -15,6 +15,7 @@ Spinlock SMP::smpLock;
 alignas(4096) static uint8_t kernelStacks[SMP::MAX_CPUS][SMP::SMP_STACK_SIZE];
 alignas(4096) static uint8_t apStacks[SMP::MAX_CPUS][SMP::SMP_STACK_SIZE];
 static uint32_t lapicIDs[SMP::MAX_CPUS] = {};
+static uint32_t apCount = 0;
 
 Atomic SMP::apReadyCount{0};
 uint32_t SMP::cpuCount = 0;
@@ -22,10 +23,6 @@ uint32_t cpuIDs[SMP::MAX_CPUS] = {};
 
 extern "C" [[noreturn]] void apMain()
 {
-	uint64_t rsp;
-	asm volatile ("mov %%rsp, %0" : "=r"(rsp));
-	Renderer::printf("[AP] Current RSP before CR3 switch: 0x%lx\n", rsp);
-
 	uint64_t cr3 = reinterpret_cast<uint64_t>(pml4) - hhdm_request.response->offset;
 	asm volatile("" ::: "memory");
 	asm volatile("mov %0, %%cr3" :: "r"(cr3) : "memory");
@@ -39,7 +36,7 @@ extern "C" [[noreturn]] void apMain()
 	GDTManager::setTSS(id, reinterpret_cast<uint64_t>(&kernelStacks[id][SMP::SMP_STACK_SIZE]));
 
 	SMP::apReadyCount.increment();
-	Renderer::printf("[AP] Core with LAPIC ID %u is online\n", id);
+	Renderer::printf("[AP] Core with LAPIC ID %u is online (CPU ID %u)\n", SMP::getLapicID(), id);
 
 	asm volatile ("sti");
 	while (true) asm volatile ("hlt");
@@ -60,28 +57,28 @@ void SMP::init()
 		return;
 	}
 
+	lapicIDs[0] = smp_request.response->bsp_lapic_id;
+	apCount = 0;
+
 	for (uint32_t i = 0; i < cpuCount; ++i)
 	{
-		if (i >= MAX_CPUS)
-		{
-			Renderer::printf("\x1b[31m[SMP] \x1b[91mToo many CPUs, ignoring core #%u\x1b[0m\n", i);
-			continue;
-		}
-
 		auto* cpu = smp_request.response->cpus[i];
-		if (!cpu)
-		{
-			Renderer::printf("\x1b[31m[SMP] \x1b[91mCore #%u is null, skipping...\x1b[0m\n", i);
-			continue;
-		}
+		if (!cpu) continue;
 
 		if (cpu->lapic_id == smp_request.response->bsp_lapic_id) continue;
-		lapicIDs[i] = cpu->lapic_id;
-		GDTManager::setTSS(i, reinterpret_cast<uint64_t>(&kernelStacks[i][SMP_STACK_SIZE]));
+		if (apCount >= MAX_CPUS - 1)
+		{
+			Renderer::printf("\x1b[31m[SMP] \x1b[91mToo many CPUs, ignoring core with LAPIC ID %u\x1b[0m\n",
+			                 cpu->lapic_id);
+			continue;
+		}
 
-		const uint64_t stackVirt = reinterpret_cast<uint64_t>(&apStacks[i][0]), stackPhys = stackVirt -
+		apCount++;
+		lapicIDs[apCount] = cpu->lapic_id;
+		GDTManager::setTSS(apCount, reinterpret_cast<uint64_t>(&kernelStacks[apCount][SMP_STACK_SIZE]));
+
+		const uint64_t stackVirt = reinterpret_cast<uint64_t>(&apStacks[apCount][0]), stackPhys = stackVirt -
 			               kernel_addr_request.response->virtual_base + kernel_addr_request.response->physical_base;
-
 		for (size_t offset = 0; offset < SMP_STACK_SIZE; offset += FrameAllocator::SMALL_SIZE)
 			if (!Paging::mapSmall(stackVirt + offset, stackPhys + offset, PageFlags::PRESENT | PageFlags::RW))
 			{
@@ -91,9 +88,11 @@ void SMP::init()
 
 		cpu->extra_argument = (stackVirt + SMP_STACK_SIZE) & ~0xFULL;
 		cpu->goto_address = reinterpret_cast<uint8_t*>(trampoline);
-		Renderer::printf("\x1b[33m[AP] \x1b[93mQueued core #%u (LAPIC %u)\x1b[0m\n", i, cpu->lapic_id);
+		Renderer::printf("\x1b[33m[AP] \x1b[93mQueued core (LAPIC ID %u) as CPU ID %u\x1b[0m\n", cpu->lapic_id,
+		                 apCount);
 	}
 
+	cpuCount = apCount + 1;
 	waitForAPs();
 }
 
@@ -102,9 +101,13 @@ uint32_t SMP::getCpuCount() { return cpuCount; }
 uint32_t SMP::getCpuID()
 {
 	const uint32_t lapic = getLapicID();
-	for (uint32_t i = 0; i < MAX_CPUS; ++i) if (lapicIDs[i] == lapic) return i;
+	for (uint32_t i = 0; i < cpuCount; ++i) if (lapicIDs[i] == lapic) return i;
 
 	Renderer::printf("[SMP] Failed to find LAPIC ID %u in CPU list!\n", lapic);
+	Renderer::printf("[SMP] Available LAPIC IDs: ");
+	for (uint32_t i = 0; i < cpuCount; ++i) Renderer::printf("%u ", lapicIDs[i]);
+	Renderer::printf("\n");
+
 	while (true) asm volatile ("hlt");
 }
 
@@ -119,14 +122,16 @@ uint32_t SMP::getLapicID()
 void SMP::waitForAPs()
 {
 	int timeout = 1000000;
+	uint32_t expectedAPs = cpuCount - 1;
 
-	Renderer::printf("\x1b[36m[SMP] \x1b[96mWaiting for APs to come online...\x1b[0m\n");
-	while (apReadyCount.load() < cpuCount - 1 && timeout--) asm volatile ("pause");
+	Renderer::printf("\x1b[36m[SMP] \x1b[96mWaiting for %u APs to come online...\x1b[0m\n", expectedAPs);
+	while (apReadyCount.load() < expectedAPs && timeout--) asm volatile ("pause");
 
 	if (timeout <= 0)
 	{
-		Renderer::printf("\x1b[31mTimeout waiting for APs! Using %u cores.\x1b[0m\n", apReadyCount.load() + 1);
+		Renderer::printf("\x1b[31mTimeout waiting for APs! Using %u cores instead of %u.\x1b[0m\n",
+		                 apReadyCount.load() + 1, cpuCount);
 		cpuCount = apReadyCount.load() + 1;
 	}
-	else Renderer::printf("\x1b[32mReady.\x1b[0m\n");
+	else Renderer::printf("\x1b[32mAll %u cores online!\x1b[0m\n", cpuCount);
 }
